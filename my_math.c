151b2b790fdfb3a2d1d48888b760c8939578927e
jijise314 <kimjihun3037@gmail.com>
2026-03-21 21:46:26 +0900

    Add files via upload

---
+++ my_math.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "my_math.h"
+
+int get_gcd(int a, int b) {
+	a = abs(a);
+	b = abs(b);
+	while (b) {
+		int temp = a % b;
+		a = b;
+		b = temp;
+	}
+	return a;
+}
+
+int get_lcm(int a, int b) {
+	if (a == 0 || b == 0) return 0;
+
+	int gcd = get_gcd(a, b);
+	return abs((a / gcd) * b);
+}
