2babfff5a3b3751fbe43c18fbe04e952bbfb66eb
jijise314 <kimjihun3037@gmail.com>
2026-04-20 18:46:37 +0900

    Add files via upload

---
+++ ch3/4.16 BONUS/SparseMatrix.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sparseMatrix.h"
+
+sparseMatrix* createSparseMatrix(int row, int col) {
+	return createArrayList(row * col);
+}
+
+int addElementSparseMatrix(sparseMatrix* sm, int row, int col, int value) {
+	int size;
+	size = sizeArrayList(sm);
+
+	insertArrayList(sm, size, (elementArrayList) {
+		row, col, value
+	});
+}
+
+int printSparseMatrix(sparseMatrix* sm) {
+	printf("희소행렬입니다.\n");
+
+	printArrayList(sm);
+}
+
+sparseMatrix* addSparseMatrix(sparseMatrix* sm1, sparseMatrix* sm2) {
+	sparseMatrix* smResult = createArrayList(100);
+
+	// i <= sizeArrayList(sm1),j <= sizeArrayList(sm2) 에서 i < sizeArrayList(sm1),j < sizeArrayList(sm2)로 수정
+	for (int i = 0; i < sizeArrayList(sm1); i++) {
+		elementArrayList nonZeroOfSM1 = getItemArrayList(sm1, i);
+
+		int j;
+		for (j = 0; j < sizeArrayList(sm2); j++) {
+			elementArrayList nonZeroOfSM2 = getItemArrayList(sm2, j);
+
+			if (nonZeroOfSM1.col == nonZeroOfSM2.col &&
+				nonZeroOfSM1.row == nonZeroOfSM2.row) {
+				insertArrayList(smResult, sizeArrayList(smResult), (elementArrayList) {
+					nonZeroOfSM1.row,
+						nonZeroOfSM1.col,
+						nonZeroOfSM1.value + nonZeroOfSM2.value
+				});
+
+				break;
+			}
+		}
+
+		if (j == sizeArrayList(sm2)) {
+			insertArrayList(smResult, sizeArrayList(smResult), nonZeroOfSM1);
+		}
+	}
+
+	return smResult;
+}
--- ch3/4.16 BONUS/main.c
+++ ch3/4.16 BONUS/main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "sparseMatrix.h"
+
+int main() {
+	sparseMatrix* sm1 = createSparseMatrix(5, 5);
+	addElementSparseMatrix(sm1, 2, 1, 10);
+	addElementSparseMatrix(sm1, 3, 3, 15);
+	addElementSparseMatrix(sm1, 4, 1, 3);
+
+	printSparseMatrix(sm1);
+
+	sparseMatrix* sm2 = createSparseMatrix(5, 5);
+	addElementSparseMatrix(sm2, 2, 1, 10);
+	addElementSparseMatrix(sm2, 3, 4, 15);
+	addElementSparseMatrix(sm2, 4, 1, 3);
+
+	printSparseMatrix(sm2);
+
+	sparseMatrix* sm3 = addSparseMatrix(sm1, sm2);
+	printf("add 직후\n");
+
+	printSparseMatrix(sm3);
+	printf("print 직후\n");
+}
