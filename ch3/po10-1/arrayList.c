4dd884a7556d4eb49b8afee8dbaa46252ee62bc5
jijise314 <kimjihun3037@gmail.com>
2026-04-21 15:21:53 +0900

    Add files via upload

---
+++ ch3/po10-1/arrayList.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arrayList.h"
+
+arrayList* createArrayList(int size) {
+	arrayList* al;
+	al = (arrayList*)malloc(sizeof(arrayList));
+
+	al->data = (elementArrayList*)malloc(
+		sizeof(elementArrayList) * size);
+	al->size = 0;
+	al->capacity = size;
+	return al;
+}
+
+void destroyArrayList(arrayList* al) {
+	free(al->data);
+	free(al);
+}
+
+int isEmptyArrayList(arrayList* al) {
+	if (al->size == 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int isFullArrayList(arrayList* al) {
+	if (al->size == al->capacity) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int sizeArrayList(arrayList* al) {
+	return al->size;
+}
+
+int insertArrayList(arrayList* al,
+	int pos, elementArrayList item) {
+	if (isFullArrayList(al)) {
+		printf("Error: 리스트가 꽉 찼습니다.\n");
+		return 0;
+	}
+	if (pos<0 || pos>al->size || item <= 0) {
+		return 0;
+	}
+	for (int i = al->size - 1; i >= pos; i--) {
+		al->data[i + 1] = al->data[i];
+	}
+
+	al->data[pos] = item;
+	al->size++;
+
+	return 1;
+}
+
+elementArrayList deleteArrayList(
+	arrayList* al, int pos) {
+	if (pos < 0 || pos >= al->size) {
+		return -1;
+	}
+
+	elementArrayList item = al->data[pos];
+
+	for (int i = pos; i < al->size - 1; i++) {
+		al->data[i] = al->data[i + 1];
+	}
+
+	al->size--;
+
+	return item;
+}
+
+int initArrayList(arrayList* al) {
+	al->size = 0;
+	return 1;
+}
+
+elementArrayList getItemArrayList(
+	arrayList* al, int pos) {
+	return al->data[pos];
+}
+
+int replaceItemArrayList(arrayList* al,
+	int pos, elementArrayList item) {
+	if (pos < 0 || pos > al->size - 1) {
+		return 0;
+	}
+
+	al->data[pos] = item;
+
+	return 1;
+}
+
+void printArrayList(arrayList* al) {
+	printf("=== 양의 정수 리스트 출력 ===\n");
+	if (al->size == 0) {
+		printf("리스트가 비어있습니다.\n");
+		return;
+	}
+	for (int i = 0; i < al->size; i++) {
+		printf("위치[%d] : %d\n", i, al->data[i]);
+	}
+}
--- ch3/po10-1/main.c
+++ ch3/po10-1/main.c
@@ -1,1 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "arrayList.h"
+
+void Menu() {
+	printf("1.추가 2.삭제 3.변경 4.출력 5.전체 삭제 0.종료\n");
+	printf("선택: ");
+}
+
+int main() {
+	arrayList* myArrayList;
+	myArrayList = createArrayList(100);
+	int choice, pos, item;
+
+	if (sizeArrayList(myArrayList) == 0) {
+		printf("Create ok\n");
+	}
+	while (1) {
+		Menu();
+		if (scanf("%d", &choice) != 1) {
+			while (getchar() != '\n');
+			continue;
+		}
+
+		switch (choice) {
+		case 1:
+			printf("추가할 양의 정수와 위치를 입력허세요.(예: 10 0): ");
+			scanf("%d %d", &item, &pos);
+			if (item <= 0) {
+				printf("=> 양의 정수를 입력하세요!");
+			}
+			else if(insertArrayList(myArrayList, pos, item)){
+				printf("=> %d에 %d가 추가되었습니다.\n", pos, item);
+			}
+			else {
+				printf("=> 잘못된 위치입니다.");
+			}
+			break;
+
+		case 2:
+			printf("삭제할 위치를 입력하세요.: ");
+			scanf("%d", &pos);
+			item = deleteArrayList(myArrayList, pos);
+			if (item < 0) {
+				printf("=> 잘못된 위치입니다.\n");
+			}
+			else {
+				printf("=> %d의 %d가 삭제되었습니다.\n", pos, item);
+			}
+			break;
+
+		case 3:
+			printf("변경할 양의 정수와 위치를 입력하세요.(예: 20 0)");
+			scanf("%d %d", &item, &pos);
+			if (item <= 0) {
+				printf("=> 양의 정수를 입력하세요!\n");
+			}
+			else if (replaceItemArrayList(myArrayList, pos, item)) {
+				printf("=> %d의 값이 %d로 변경되었습니다.\n", pos, item);
+			}
+			else {
+				printf("=> 잘못된 위치입니다.");
+			}
+			break;
+			
+		case 4:
+			printArrayList(myArrayList);
+			break;
+			
+		case 5:
+			initArrayList(myArrayList);
+			printf("=> 리스트의 모든 데이터가 삭제되었습니다.\n");
+			break;
+
+		case 0:
+			destroyArrayList(myArrayList);
+			printf("프로그램 종료!");
+			return 0;
+
+		default:
+			printf("=> 잘못된 입력입니다. 다시 선택하세요.\n");
+		}
+	}
+	return 0;
+}
